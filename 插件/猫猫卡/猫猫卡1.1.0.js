71af42f08fd2c5136a4411758c4ae9078dcba580
Sheyiyuan <2125107118@qq.com>
2024-06-08 15:52:59 +0800

    猫猫卡更新优化

--- 插件/猫猫卡/猫猫卡1.1.0.js
+++ 插件/猫猫卡/猫猫卡1.1.0.js
@@ -43,10 +43,11 @@ cmdCoccat.solve = (ctx, msg, cmdArgs) => {
       return ret;
     }
     default: {
-      seal.replyToSender(ctx, msg, `${ctx.player.name}老师的七版COC猫猫作成:`);
+      let result = ``
       let att = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
       if (!val) val = 1;
       for (let i = 0; i < val; i++) {
+        result = ``
         att[0] = D(1, 3, 5)
         att[1] = D(2, 6, 5, 14)
         att[2] = D(2, 6, 5, 6)
@@ -57,7 +58,11 @@ cmdCoccat.solve = (ctx, msg, cmdArgs) => {
         att[7] = D(2, 6, 5, 3)
         att[8] = Math.floor((att[3] + 5) / 10)
         att[9] = D(3, 6, 5)
-        seal.replyToSender(ctx, msg, `力量:${att[0]} 敏捷:${att[1]} 意志:${att[2]}\n体质:${att[3]} 外形:${att[4]} 教养:${att[5]}\n体型:${att[6]} 智力:${att[7]}\nHP:${att[8]} 幸运:${att[9]}`);
+        if (!i) {
+          result += `${ctx.player.name}老师的七版COC猫猫作成:\n`
+        }
+        result += `力量:${att[0]} 敏捷:${att[1]} 意志:${att[2]}\n体质:${att[3]} 外形:${att[4]} 教养:${att[5]}\n体型:${att[6]} 智力:${att[7]}\nHP:${att[8]} 幸运:${att[9]}`
+        seal.replyToSender(ctx, msg, result);
       }
     }
       return seal.ext.newCmdExecuteResult(true);
